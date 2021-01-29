c8ec5b01e68850cf71a1c92d128b931d093c71b6
Riku <smile6280@icloud.com>
2021-01-30 00:12:15 +0900

    change css>bem

--- js/script.js
+++ js/script.js
@@ -3,45 +3,26 @@ $(function(){
     // SP
     if (window.matchMedia( '(max-width: 959px)' ).matches) {
         $(function(){
-                // workSlide
-    $('.work-slider').slick({
-        lazyLoad: 'ondemand',
-        centerMode: true,
-        centerPadding: '15%',
-        arrows: false
-    });
+            // workSlide
+            $('.work--slider').slick({
+                lazyLoad: 'ondemand',
+                centerMode: true,
+                centerPadding: '15%',
+                arrows: false
+            });
 
-    // langSlide
-    $('.lang-slider').slick({
-        lazyLoad: 'progressive',
-        centerMode: true,
-        centerPadding: '15%',
-        arrows: false
-    });
-
-    // environmentTabs
-    //タブクリック時
-    $(".environment-nav").click(function() {
-        //何個目のタブをクリックしたか
-        var index = $('.environment-nav').index(this);
-
-        //コンテンツを一旦非表示
-        $('.environment-content').hide();
-
-        //クリックしたタブに応じてコンテンツを表示
-        $('.environment-content').eq(index).fadeIn();
-
-        //タブに付与したクラス(act)を除去
-        $('.environment-nav').removeClass('environment-active');
-
-        //クリックしたタブにクラス(act)をつけます。
-        $(this).addClass('environment-active')
-    });
+            // langSlide
+            $('.lang--slider').slick({
+                lazyLoad: 'progressive',
+                centerMode: true,
+                centerPadding: '15%',
+                arrows: false
+            });
         });
     } else {
         // PC
         // workSlide
-        $('.work-slider').slick({
+        $('.work--slider').slick({
             lazyLoad: 'ondemand',
             centerMode: true,
             centerPadding: '25%',
@@ -50,31 +31,31 @@ $(function(){
         });
 
         // langSlide
-        $('.lang-slider').slick({
+        $('.lang--slider').slick({
             lazyLoad: 'progressive',
             centerMode: true,
             centerPadding: '25%',
             prevArrow:'<div class="prev">PREV</div>',
     　　    nextArrow:'<div class="next">NEXT</div>'
         });
-
-        // environmentTabs
-        //タブクリック時
-        $(".environment-nav").click(function() {
-            //何個目のタブをクリックしたか
-            var index = $('.environment-nav').index(this);
-
-            //コンテンツを一旦非表示
-            $('.environment-content').hide();
-
-            //クリックしたタブに応じてコンテンツを表示
-            $('.environment-content').eq(index).fadeIn();
-
-            //タブに付与したクラス(act)を除去
-            $('.environment-nav').removeClass('environment-active');
-
-            //クリックしたタブにクラス(act)をつけます。
-            $(this).addClass('environment-active')
-        });
     }
+
+    // environmentTabs
+    //タブクリック時
+    $(".environment--nav").click(function() {
+        //何個目のタブをクリックしたか
+        var index = $('.environment--nav').index(this);
+
+        //コンテンツを一旦非表示
+        $('.environment--content').hide();
+
+        //クリックしたタブに応じてコンテンツを表示
+        $('.environment--content').eq(index).fadeIn();
+
+        //タブに付与したクラス(act)を除去
+        $('.environment--nav').removeClass('isActive');
+
+        //クリックしたタブにクラス(act)をつけます。
+        $(this).addClass('isActive')
+    });
 });
